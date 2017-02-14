0bd1f640fefbff31b928c3bab0fedd5e02db8379
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2017-02-14 20:48:53 +0000

    better comments for ReadCommitsIgnoringErrors

--- src/org/vcsreader/ReadCommitsIgnoringErrors.java
+++ src/org/vcsreader/ReadCommitsIgnoringErrors.java
@@ -7,28 +7,33 @@ import org.vcsreader.vcs.git.GitVcsRoot;
 import java.io.File;
 
 public class ReadCommitsIgnoringErrors {
+	/**
+	 * Sometimes it might be desirable to continue execution even if it has partially failed.
+	 * In real world, VCS quite often can fail on some commits because of data corruption.
+	 * Below one VCS roots has invalid URL to simulate failure.
+	 */
 	public static void main(String[] args) {
-		// Sometimes it might be desirable to continue executing action even if some of them fail.
-		// For example, below is VCS root with invalid URL.
-		// In real world, VCS might always fail on some commits because of data corruption.
 		GitSettings settings = GitSettings.defaults().withFailFast(false);
 		VcsProject vcsProject = new VcsProject(
 				new GitVcsRoot(".junit4-clone", "https://github.com/junit-team/junit4", settings),
 				new GitVcsRoot(".invalid-project", "invalid.url", settings)
 		);
 
+
+		System.out.println("Cloning repositories...");
 		for (VcsRoot vcsRoot : vcsProject.vcsRoots()) {
 			if (new File(vcsRoot.repoFolder()).exists()) continue;
-
 			System.out.println("Cloning " + vcsRoot.repoUrl());
+
 			CloneResult cloneResult = vcsRoot.cloneIt();
 
 			if (!cloneResult.isSuccessful()) {
 				for (Exception e : cloneResult.exceptions()) {
-					System.out.println("Clone error: '" + e.getMessage() + "'");
+					System.out.println("Clone error: \"" + e.getMessage().trim() + "\"");
 				}
 			}
 		}
+
 
 		System.out.println("Reading commits...");
 		LogResult logResult = vcsProject.log(TimeRange.beforeNow());
